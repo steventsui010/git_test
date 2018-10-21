9ba185c02e5200ae5eafbe0e104ad0402101aeff
SaSa's zone <sasa@SaSass-MacBook-Air.local>
2018-10-21 15:43:59 +0800

    new callback.c

--- callback.c
+++ callback.c
@@ -8,10 +8,3 @@ void send_email(char *content, char *subject, char *dest_address);
 void send_txtmsg(char *content, char *subject, char *dest_address);
 
 void exception_notify(, void (*)(char *, char *, char *), );
-
-
-int main() 
-{
-	
-
-
